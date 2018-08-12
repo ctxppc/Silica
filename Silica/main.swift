0b359487d3f7db3c6888d7e838aca0ba87aeadc8
Constantino Tsarouhas <ctxppc@gmail.com>
2018-08-12 19:40:35 +0200

    Initial Commit

---
+++ Silica/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Silica
+//
+//  Created by Constantino Tsarouhas on 12/08/2018.
+//  Copyright © 2018 Creatunit. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
